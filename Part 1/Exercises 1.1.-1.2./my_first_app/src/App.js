cd5d866659f4279d5a326a68d2eb2aeeec7735f3
otimbrian <otims.brian@gmail.com>
2021-10-01 17:56:05 +0300

    Complete part 0 and part 1 of the fullstack exercise

---
+++ Part 1/Exercises 1.1.-1.2./my_first_app/src/App.js
@@ -0,0 +1,45 @@
+import React from "react";
+
+const Header = (props) => {
+  return(
+    <div><h1>{props.head}</h1></div>
+  )
+}
+
+const Content = (props) => {
+  return(
+    <div>
+      <h1>{props.part} {props.exercise}</h1>
+    </div>
+  )
+}
+
+const Total = (props) => {
+  return(
+    <div>
+    <p>Number of exercises {props.first + props.second + props.third}</p>
+    </div>
+  )
+}
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  return(
+    <div>
+      <Header head={course} />
+      <Content part={part1} exercise={exercises1} />
+      <Content part={part2} exercise={exercises2} />
+      <Content part={part3} exercise={exercises3} />
+      <Total first={exercises1} second={exercises2} third={exercises3} />
+    </div>
+  )
+}
+
+export default App;
