b7d83c97c052aff2a798fed048b4b4be2fddda40
otimbrian <otims.brian@gmail.com>
2021-10-09 00:40:19 +0300

    Complete part 1

--- Part 1/Exercises 1.11*-1.14*/my_first_app/src/App.js
+++ Part 1/Exercises 1.11*-1.14*/my_first_app/src/App.js
@@ -1,72 +1,52 @@
-import { useState } from "react"
-
-const Message = ({text})=> <div><h1>{text}</h1></div>
-const Button = ({action, text}) => <button onClick={action}>{text}</button>
-const Infor = ({text, value}) => {
-    if(text === 'Positive'){
-        return(
-            <tr>
-                <td>{text}</td>
-                <td>{value}%</td>
-            </tr>
-        )
-    }
-    return(
-        <tr>
-            <td>{text}</td>
-            <td>{value}</td>
-        </tr>
-    )
-}
-
-const Statistics = ({good, neutral, bad}) =>{
-    if(good===0 & neutral===0 & bad===0){
-        return(
-            <div><h4>No Feedback Given</h4></div>
-        )
-    }
-    return(
-        <div>
-            <table>
-            <tbody>
-                <Infor text='Good' value={good} />
-                <Infor text='Neutral' value={neutral} />
-                <Infor text ='Bad' value ={bad} />
-                <Infor text='All' value={bad + neutral + good} />
-                <Infor text='Average' value={((bad * -1) + good)/(bad + neutral + good)} />
-                <Infor text='Positive' value={good /(bad + neutral + good) * 100} />
-            </tbody>
-            </table>
-        </div>
-    )
-}
+import React, { useState } from 'react'
 
 
-const App = (props) => {
+const Button = ({text, action}) => <button onClick={action}>{text}</button>
+const Heading = ({text}) => <h2>{text}</h2>
 
-    const [feedback, setFeedback] = useState({'good': 0, 'neutral': 0, 'bad': 0})
-    const handleGood = () => setFeedback({...feedback, good: feedback.good + 1})
-    const handleNeutral = () => setFeedback({...feedback, neutral: feedback.neutral + 1})
-    const handleBad = () => setFeedback({...feedback, bad: feedback.bad + 1})
-    
-    return(
+
+const App = () => {
+  const anecdotes = [
+    'If it hurts, do it more often',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients'
+  ]
+   
+  const [selected, setSelected] = useState(0)
+  const [vote, setVote] = useState(Array.apply(null, new Array(anecdotes.length)).map(Number.prototype.valueOf, 0))
+  const gettingValue = () => setSelected((Math.floor(Math.random() * anecdotes.length)))
+  const addingVote = () => {
+        const newVote = [...vote]
+        newVote[selected] += 1
+        setVote(newVote)
+  }
+  
+
+  return (
     <div>
+
         <div>
-            <Message text ='Give Freedback' />
+            <Heading text='Anectode Of The Day' />
+            <h4>{anecdotes[selected]}</h4>
+            <h6>This has {vote[selected]} Votes</h6>
+        </div>
+
+        <div>
+            <Button action={addingVote} text='Vote' />
+            <Button action={gettingValue} text='Get Next Anectode' />
         </div>
         <div>
-            <Button action={handleGood} text='Good' />
-            <Button action={handleNeutral} text='Neutral' />
-            <Button action={handleBad} text='Bad' />
+            <Heading text='Anectode With Most Votes' />
+            <h4>{anecdotes[vote.indexOf(Math.max(...vote))]}</h4>
+            <h6>This has {Math.max(...vote)} Votes</h6>
         </div>
-        <div>
-            <Message text = 'Statistic' />
-        </div>
-        <Statistics good={feedback.good} neutral={feedback.neutral} bad={feedback.bad} />
         
     </div>
   )
 }
 
-
-export default App;
+export default App
