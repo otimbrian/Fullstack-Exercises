c39e8465df9aa3ece335790fbcfdfcf9c0ad69ec
otimbrian <otims.brian@gmail.com>
2022-01-04 16:49:13 +0300

    Completed controllers

--- Part 4/blog list/index.js
+++ Part 4/blog list/index.js
@@ -6,15 +6,7 @@ const midddleware = require('./utils/middleware')
 const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
-
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
+const blogsRouter = require('./controllers/blogs')
 
 mongoose.connect(config.mongoUrl)
 
@@ -22,23 +14,7 @@ app.use(cors())
 app.use(express.json())
 app.use(midddleware.requestLogger)
 
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
+app.use(blogsRouter)
 
 app.use(midddleware.unknownEndPoint)
 app.use(midddleware.errorHandler)
