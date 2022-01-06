e894442a0673bc37826b08e9d5398066dfec6dde
otimbrian <otims.brian@gmail.com>
2022-01-06 21:41:43 +0300

    Started work on testing

---
+++ Part 4/blog list/test/listhelper.test.js
@@ -0,0 +1,134 @@
+const listHelper = require('../utils/list_helper')
+
+const singleBlog = [
+    {
+        'title': 'Welcome to Nature',
+        'author': 'Otim Brian',
+        'url': 'http://localhost',
+        'likes': 20,
+        'id': '61d438f5f64dcc7493e89c6a'
+    }
+]
+const multipleBlogs = [
+    {
+        'title': 'Welcome to Nature',
+        'author': 'Otim Brian',
+        'url': 'http://localhost',
+        'likes': 20,
+        'id': '61d438f5f64dcc7493e89c6a'
+    },
+    {
+        'title': 'This is the world',
+        'author': 'Edwin Huble',
+        'url': 'http://localhost/getting-to-the-stars',
+        'likes': 50,
+        'id': '61d43941f64dcc7493e89c6c'
+    },
+    {
+        'title': 'Nas The GOAT',
+        'author': 'DJ Danny The Don',
+        'url': 'http://localhost/getting-nas',
+        'likes': 23,
+        'id': '61d44636dbf4a54bc01e034e'
+    },
+    {
+        'title': 'React.JS for web',
+        'author': 'Denny Jolly',
+        'url': 'http://localhost/web-frame-work',
+        'likes': 15,
+        'id': '61d469a2a667a36b77c0ff94'
+    }
+]
+const multipleBlogsWithMoreThanOneFavourite = [[
+    {
+        'title': 'Welcome to Nature',
+        'author': 'Otim Brian',
+        'url': 'http://localhost',
+        'likes': 20,
+        'id': '61d438f5f64dcc7493e89c6a'
+    },
+    {
+        'title': 'This is the world',
+        'author': 'Edwin Huble',
+        'url': 'http://localhost/getting-to-the-stars',
+        'likes': 50,
+        'id': '61d43941f64dcc7493e89c6c'
+    },
+    {
+        'title': 'Nas The GOAT',
+        'author': 'DJ Danny The Don',
+        'url': 'http://localhost/getting-nas',
+        'likes': 50,
+        'id': '61d44636dbf4a54bc01e034e'
+    },
+    {
+        'title': 'React.JS for web',
+        'author': 'Denny Jolly',
+        'url': 'http://localhost/web-frame-work',
+        'likes': 15,
+        'id': '61d469a2a667a36b77c0ff94'
+    }
+]]
+describe('Dummy Test', () => {
+    test('[] should be 1', () => {
+        expect(listHelper.dummy([])).toBe(1)
+    })
+
+})
+
+describe('Total likes of ', () => {
+    test('[] should be zero ', () => {
+        expect(listHelper.totalLike([])).toBe(0)
+    })
+
+    test('A single blog should be equal to likes of that list', () => {
+        expect(listHelper.totalLike(singleBlog)).toBe(20)
+    })
+    test('Multibe blogs should be calculated right', () => {
+        expect(listHelper.totalLike(multipleBlogs)).toBe(108)
+    })
+
+})
+
+const resultForMultipleBlog = [
+    {
+        'title': 'This is the world',
+        'author': 'Edwin Huble',
+        'url': 'http://localhost/getting-to-the-stars',
+        'likes': 50,
+        'id': '61d43941f64dcc7493e89c6c'
+    }
+]
+const resultForMultipleBlogWwithMorethanOneFavourites = [
+    {
+        'title': 'This is the world',
+        'author': 'Edwin Huble',
+        'url': 'http://localhost/getting-to-the-stars',
+        'likes': 50,
+        'id': '61d43941f64dcc7493e89c6c'
+    },
+    {
+        'title': 'Nas The GOAT',
+        'author': 'DJ Danny The Don',
+        'url': 'http://localhost/getting-nas',
+        'likes': 50,
+        'id': '61d44636dbf4a54bc01e034e'
+    }
+]
+
+describe('Favourite Blogs of', () => {
+    test('[] should be zero', () => {
+        expect(listHelper.favouriteBlog([])).toEqual([])
+    })
+
+    test('A single blog should return that blog', () => {
+        expect(listHelper.favouriteBlog(singleBlog)).toEqual(singleBlog)
+    })
+
+    test('Multiple Blogs should be right', () => {
+        expect(listHelper.favouriteBlog(multipleBlogs)).toEqual(resultForMultipleBlog)
+    })
+    test('Multiple Blogs with more than one favourite should return all favourite', () => {
+        expect(listHelper.favouriteBlog(multipleBlogsWithMoreThanOneFavourite)).toEqual(resultForMultipleBlogWwithMorethanOneFavourites)
+    })
+})
---
+++ Part 4/blog list/utils/list_helper.js
@@ -0,0 +1,36 @@
+
+// eslint-disable-next-line no-unused-vars
+const dummy = (blogs) => {
+    return 1
+}
+
+const totalLike = (blogs) => {
+    var sum = 0
+    blogs.forEach(blog => {
+        sum += blog.likes
+    })
+    return blogs.length === 0
+        ? 0
+        : sum
+}
+
+
+const favouriteBlog = (blogs) => {
+    var value = 0
+    blogs.forEach(
+        blog => {
+            if(blog.likes > value){
+                value = blog.likes
+            }
+        }
+    )
+    console.log(value)
+    const result =  blogs.filter(
+        blog => {
+            return blog.likes === value
+        }
+    )
+    console.log(result)
+    return result
+}
+module.exports = { dummy, totalLike, favouriteBlog }
