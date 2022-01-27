71d3b340ea87fc501bb47da2a1a89a620a278c40
otimbrian <otims.brian@gmail.com>
2022-01-27 03:14:51 +0300

    Finished 4.13 and 4.14

--- Part 4/blog list/controllers/blogs.js
+++ Part 4/blog list/controllers/blogs.js
@@ -8,18 +8,9 @@ blogsRouter.get('/', async (request, response, next) => {
     }catch{
         exception => next(exception)
     }
-    // Blog
-    //     .find({})
-    //     .then(blogs => {
-    //         response.json(blogs)
-    //     })
-    //     .catch(
-    //         error => next(error)
-    //     )
 })
 
 blogsRouter.post('/', async (request, response) => {
-    // const blog = new Blog(request.body)
     const body = request.body
 
     const blog = new Blog({
@@ -29,21 +20,28 @@ blogsRouter.post('/', async (request, response) => {
         'likes' : body.likes
     })
 
-    // try{
     const savedBlog = await blog.save()
     response.status(201).json(savedBlog)
-    // }catch{
-    //     exception => next(exception)
-    // }
+})
 
-    // blog
-    //     .save()
-    //     .then(result => {
-    //         response.status(201).json(result)
-    //     })
-    //     .catch(
-    //         error => next(error)
-    //     )
+blogsRouter.delete('/:id', async (request, response) => {
+    await Blog.findByIdAndDelete(request.params.id)
+    response.status(204).end()
+})
+
+blogsRouter.put('/:id', async (request, response) => {
+    const body = request.body
+
+    const blog = {
+        'title' : body.title,
+        'author' : body.author,
+        'url' : body.url,
+        'likes' : body.likes
+    }
+    console.log(blog)
+
+    const upDatedBlogs = await Blog.findByIdAndUpdate(request.params.id, blog, { new : true })
+    response.json(upDatedBlogs.toJSON())
 })
 
 module.exports = blogsRouter
