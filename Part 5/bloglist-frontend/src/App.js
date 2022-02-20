15bfb0817ce47c424930c39a8e9a3efe42d61d23
otimbrian <otims.brian@gmail.com>
2022-02-20 03:18:24 +0300

    Finished 5.3

--- Part 5/bloglist-frontend/src/App.js
+++ Part 5/bloglist-frontend/src/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import Blog from './components/Blog'
+import AddBlog from './components/AddBlog'
 import blogService from './services/blogs'
 import loginServices from './services/login'
 import './index.css'
@@ -9,6 +10,9 @@ const App = () => {
   const [blogs, setBlogs] = useState([])
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
+  const [title, setTitle] = useState('')
+  const [author, setAuthor] = useState('')
+  const [url, setUrl] = useState('')
   const [user, setUser] = useState(null)
 
   useEffect(() => {
@@ -21,6 +25,7 @@ const App = () => {
       const loggedUser = window.localStorage.getItem('loggedInUser')
       if(loggedUser){
           const user = JSON.parse(loggedUser)
+          blogService.setToken(user.token)
           setUser(user)
       }
 
@@ -34,6 +39,7 @@ const App = () => {
             window.localStorage.setItem(
                 'loggedInUser', JSON.stringify(user)
             )
+            blogService.setToken(user.token)
             setUser(user)
             setUsername('')
             setPassword('')
@@ -52,7 +58,23 @@ const App = () => {
       window.localStorage.removeItem('loggedInUser')
       setUser(null)
   }
+  const handleTitle = event => setTitle(event.target.value)
+  const handleAuthor = event => setAuthor(event.target.value)
+  const handleUrl = event => setUrl(event.target.value)
+  const addBlog = async (event) => {
+      event.preventDefault()
+      const newBlog = {
+            title: title,
+            author: author,
+            url: url,
+      }
 
+      const blog = await blogService.create(newBlog)
+      setBlogs(blogs.concat(blog))
+      setTitle('')
+      setAuthor('')
+      setUrl('')
+  }
 
   if(user === null){
     return(
@@ -78,7 +100,11 @@ const App = () => {
         <h3>{user.name} is logged in </h3>
         <button onClick={handleLogout}>Logout</button>
         <hr />
-
+        <div>
+            <AddBlog handleSubmit={addBlog} handleAuthorChange={handleAuthor}
+                handleTitleChange={handleTitle} handleUrlChange={handleUrl} 
+                title={title} author={author} url={url} />
+        </div>
         <h2>Blogs</h2>
         {blogs.map(blog =>
             <Blog key={blog.id} blog={blog} />
---
+++ Part 5/bloglist-frontend/src/components/AddBlog.js
@@ -0,0 +1,31 @@
+import React from "react"
+
+
+const AddBlog = ({handleSubmit, handleTitleChange, handleAuthorChange, handleUrlChange,
+        title, author, url}) => {
+    return(
+        <form onSubmit={handleSubmit}>
+            <div>
+                Title :
+                <input type="text" 
+                    value={title} name="Title" 
+                    onChange={handleTitleChange}/>
+            </div>
+            <div>
+                Author : 
+                <input type="text" 
+                    value={author} name="Title" 
+                    onChange={handleAuthorChange} />
+            </div>
+            <div>
+                Url :
+                <input type="text" 
+                    value={url} name="Url"
+                    onChange={handleUrlChange} />
+            </div>
+            <button type="submit">Add Blog</button>
+        </form>
+    )
+}
+
+export default AddBlog
