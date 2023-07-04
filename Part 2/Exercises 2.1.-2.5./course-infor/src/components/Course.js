ab1c2c921ef7810c30f7840a339726ab8b5d4fbe
Otim Brian <otims.brian@gmail.com>
2023-07-05 01:45:07 +0300

    added changes

--- Part 2/Exercises 2.1.-2.5./course-infor/src/components/Course.js
+++ Part 2/Exercises 2.1.-2.5./course-infor/src/components/Course.js
@@ -1,39 +1,39 @@
 import React from "react";
 
-const Course = ({couses}) => {
-   /* const calculateTotal = (course) => {
-        return(
-            course.parts.reduce(function (sum, part) {
-                            return (sum + part.exercises);
-                        }, 0)
-        )
-    }*/
-    return(
-    couses.map(
-        (course) => {
-            return(
-                <div>
-                    <h1>{course.name}</h1>
+const Course = ({ couses }) => {
+    /* const calculateTotal = (course) => {
+         return(
+             course.parts.reduce(function (sum, part) {
+                             return (sum + part.exercises);
+                         }, 0)
+         )
+     }*/
+    return (
+        couses.map(
+            (course) => {
+                return (
                     <div>
-                        {
-                            course.parts.map(
-                                function content(part){
-                                    return(
-                                        <li key={part.id}>{part.name} : {part.exercises}</li>
-                                    )
-                                }
-                            )
-                        }
+                        <h1>{course.name}</h1>
+                        <div>
+                            {
+                                course.parts.map(
+                                    function content(part) {
+                                        return (
+                                            <li key={part.id}>{part.name} : {part.exercises}</li>
+                                        )
+                                    }
+                                )
+                            }
+                        </div>
+                        <h5>Total of {
+                            course.parts.reduce(function (sum, part) {
+                                return (sum + part.exercises);
+                            }, 0)
+                        } Exercises</h5>
                     </div>
-                    <h5>Total of {
-                        course.parts.reduce(function (sum, part) {
-                            return (sum + part.exercises);
-                        }, 0)
-                    } Exercises</h5> 
-                </div>
-            )
-        }
-    )
+                )
+            }
+        )
     )
 }
 
